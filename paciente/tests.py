2c5172b0c481915b07ad8dcd8d8dae545da9b67f
Veríssimo Terceiro <terceiroverissimo@gmail.com>
2024-04-19 19:22:55 -0300

    testa agendar horario

--- paciente/tests.py
+++ paciente/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import DatasAbertas, Consulta
 
-# Create your tests here.
+
+class AgendarHorarioTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username="testuser", password="12345")
+        self.data_aberta = DatasAbertas.objects.create(
+            agendado=False, data="2021-01-01 12:00:00", user=self.user
+        )
+        self.url = reverse("agendar_horario", args=[self.data_aberta.id])
+
+    def test_agendar_horario(self):
+        self.client.login(username="testuser", password="12345")
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(
+            Consulta.objects.filter(
+                paciente=self.user, data_aberta=self.data_aberta
+            ).exists()
+        )
+        self.data_aberta.refresh_from_db()
+        self.assertTrue(self.data_aberta.agendado)
